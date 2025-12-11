a6e40ccb7f825ebc098123d0390b30e64ff47592
alekseystrunets <a.strunetss@gmail.com>
2025-12-11 15:29:19 +0100

    Initial Commit

---
+++ InnoNet/InnoNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnoNetApp.swift
+//  InnoNet
+//
+//  Created by user on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct InnoNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
